39a7dcca1527a253090a469d19d159c4c70b2bd5
Anastasia Listopadova <nalist15@gmail.com>
2019-12-12 17:02:02 +0300

    Lab_Work №6

---
+++ Lab_Work №6/Program_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise__5
+{
+    public partial class Socks_With_Bears
+    {
+        public void Success()
+        {
+            Console.WriteLine("Носок с мишками найден!\n");
+        }
+    }
+}
